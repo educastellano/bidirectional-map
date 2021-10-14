a9130c9e7c3ec89bdcfc1b005882aaf825fc2a71
Eduard Castellano <educastellano08@gmail.com>
2021-10-14 11:24:07 +0200

    Use 2 spaces tabs

--- index.js
+++ index.js
@@ -1,87 +1,87 @@
 
 export default class BidirectionalMap {
-    constructor(object=null) {
-        this._map = new Map()
-        this._reverse = new Map()
-        if (object) {
-            for (let attr in object) {
-                if ({}.hasOwnProperty.call(object, attr)) {
-                    this.set(attr, object[attr])
-                }
-            }
-        }
+  constructor(object=null) {
+    this._map = new Map()
+    this._reverse = new Map()
+    if (object) {
+      for (let attr in object) {
+          if ({}.hasOwnProperty.call(object, attr)) {
+            this.set(attr, object[attr])
+          }
+      }
     }
-    get size () {
-        return this._map.size
+  }
+  get size () {
+    return this._map.size
+  }
+  set(key, value) {
+    if (this._map.has(key)) {
+      let _value = this._map.get(key)
+      this._reverse.delete(_value)
     }
-    set(key, value) {
-        if (this._map.has(key)) {
-            let _value = this._map.get(key)
-            this._reverse.delete(_value)
-        }
-        if (this._reverse.has(value)) {
-            let _key = this._reverse.get(value)
-            this._map.delete(_key)
-        }
-        this._map.set(key, value)
-        this._reverse.set(value, key)
+    if (this._reverse.has(value)) {
+      let _key = this._reverse.get(value)
+      this._map.delete(_key)
     }
-    get(key) {
-        return this._map.get(key)
+    this._map.set(key, value)
+    this._reverse.set(value, key)
+  }
+  get(key) {
+    return this._map.get(key)
+  }
+  getKey(value) {
+    return this._reverse.get(value)
+  }
+  clear() {
+    this._map.clear()
+    this._reverse.clear()
+  }
+  delete(key) {
+    let value = this._map.get(key)
+    this._map.delete(key)
+    this._reverse.delete(value)
+  }
+  deleteValue(value) {
+    let key = this._reverse.get(value)
+    this._map.delete(key)
+    this._reverse.delete(value)
+  }
+  entries() {
+    return this._map.entries()
+  }
+  has(key) {
+    return this._map.has(key)
+  }
+  hasValue(value) {
+    return this._reverse.has(value)
+  }
+  keys() {
+    return this._map.keys()
+  }
+  values() {
+    return this._map.values()
+  }
+  getObject() {
+    for (const key of this._map.keys()) {
+      if (!isPrimitive(key)) {
+        throw new Error('There are non-primitive keys')
+      }
     }
-    getKey(value) {
-        return this._reverse.get(value)
+    return Object.fromEntries(this._map.entries())
+  }
+  getObjectReverse() {
+    for (const key of this._reverse.keys()) {
+      if (!isPrimitive(key)) {
+        throw new Error('There are non-primitive keys')
+      }
     }
-    clear() {
-        this._map.clear()
-        this._reverse.clear()
-    }
-    delete(key) {
-        let value = this._map.get(key)
-        this._map.delete(key)
-        this._reverse.delete(value)
-    }
-    deleteValue(value) {
-        let key = this._reverse.get(value)
-        this._map.delete(key)
-        this._reverse.delete(value)
-    }
-    entries() {
-        return this._map.entries()
-    }
-    has(key) {
-        return this._map.has(key)
-    }
-    hasValue(value) {
-        return this._reverse.has(value)
-    }
-    keys() {
-        return this._map.keys()
-    }
-    values() {
-        return this._map.values()
-    }
-    getObject() {
-        for (const key of this._map.keys()) {
-            if (!isPrimitive(key)) {
-                throw new Error('There are non-primitive keys')
-            }
-        }
-        return Object.fromEntries(this._map.entries())
-    }
-    getObjectReverse() {
-        for (const key of this._reverse.keys()) {
-            if (!isPrimitive(key)) {
-                throw new Error('There are non-primitive keys')
-            }
-        }
-        return Object.fromEntries(this._reverse.entries())
-    }
+    return Object.fromEntries(this._reverse.entries())
+  }
 }
 
 function isPrimitive(value) {
-    return (
-        value === null || 
-        !(typeof value === 'object' || typeof value === 'function')
-    )
+  return (
+    value === null || 
+    !(typeof value === 'object' || typeof value === 'function')
+  )
 }
--- test.js
+++ test.js
@@ -2,148 +2,148 @@ var test = require('tape');
 var BiMap = require('./dist/index').default
 
 var makeOne = function() {
-    return new BiMap({
-        john: 'mary',
-        bob: 'alice',
-        ptolemy: 'cleopatra',
-    })
+  return new BiMap({
+    john: 'mary',
+    bob: 'alice',
+    ptolemy: 'cleopatra',
+  })
 }    
 
 test('it should create an empty Map', function (t) {
-    var empty = new BiMap()
-    t.equals(empty instanceof BiMap, true);
-    t.equals(empty.size, 0);
-    t.end();
+  var empty = new BiMap()
+  t.equals(empty instanceof BiMap, true);
+  t.equals(empty.size, 0);
+  t.end();
 });
 
 test('it should get the value by key', function (t) {
-    var monogamy = makeOne()
-    t.equals(monogamy.get('bob'), 'alice');
-    t.end();
+  var monogamy = makeOne()
+  t.equals(monogamy.get('bob'), 'alice');
+  t.end();
 });
 
 test('it should get the key by value', function (t) {
-    var monogamy = makeOne()
-    t.equals(monogamy.getKey('alice'), 'bob');
-    t.end();
+  var monogamy = makeOne()
+  t.equals(monogamy.getKey('alice'), 'bob');
+  t.end();
 });
 
 test('it should set change values of same key', function (t) {
-    var monogamy = new BiMap()
-    monogamy.set('a', 0)
-    monogamy.set('a', 1)
-    t.equals(monogamy.get('a'), 1);
-    t.equals(monogamy.getKey(1), 'a');
-    t.equals(monogamy.hasValue(0), false);
-    t.equals(monogamy.hasValue(1), true);
-    t.end();
+  var monogamy = new BiMap()
+  monogamy.set('a', 0)
+  monogamy.set('a', 1)
+  t.equals(monogamy.get('a'), 1);
+  t.equals(monogamy.getKey(1), 'a');
+  t.equals(monogamy.hasValue(0), false);
+  t.equals(monogamy.hasValue(1), true);
+  t.end();
 });
 
 test('it should set change keys of same value', function (t) {
-    var monogamy = new BiMap()
-    monogamy.set('a', 0)
-    monogamy.set('b', 0)
-    t.equals(monogamy.get('b'), 0);
-    t.equals(monogamy.getKey(0), 'b');
-    t.equals(monogamy.has('a'), false);
-    t.equals(monogamy.has('b'), true);
-    t.end();
+  var monogamy = new BiMap()
+  monogamy.set('a', 0)
+  monogamy.set('b', 0)
+  t.equals(monogamy.get('b'), 0);
+  t.equals(monogamy.getKey(0), 'b');
+  t.equals(monogamy.has('a'), false);
+  t.equals(monogamy.has('b'), true);
+  t.end();
 });
 
 test('it should set the same key-value', function (t) {
-    var monogamy = new BiMap()
-    monogamy.set('a', 0)
-    monogamy.set('a', 0)
-    t.equals(monogamy.get('a'), 0);
-    t.equals(monogamy.getKey(0), 'a');
-    t.equals(monogamy.has('a'), true);
-    t.equals(monogamy.hasValue(0), true);
-    t.end();
+  var monogamy = new BiMap()
+  monogamy.set('a', 0)
+  monogamy.set('a', 0)
+  t.equals(monogamy.get('a'), 0);
+  t.equals(monogamy.getKey(0), 'a');
+  t.equals(monogamy.has('a'), true);
+  t.equals(monogamy.hasValue(0), true);
+  t.end();
 });
 
 
 test('it should get the size', function (t) {
-    var monogamy = makeOne()
-    t.equals(monogamy.size, 3);
-    t.end();
+  var monogamy = makeOne()
+  t.equals(monogamy.size, 3);
+  t.end();
 });
 
 test('it should clear', function (t) {
-    var monogamy = makeOne()
-    monogamy.clear()
-    t.equals(monogamy.size, 0);
-    t.end();
+  var monogamy = makeOne()
+  monogamy.clear()
+  t.equals(monogamy.size, 0);
+  t.end();
 });
 
 test('it should delete by key', function (t) {
-    var monogamy = makeOne()
-    monogamy.delete('john')
-    t.equals(monogamy.has('john'), false);
-    t.equals(monogamy.hasValue('mary'), false);
-    t.end();
+  var monogamy = makeOne()
+  monogamy.delete('john')
+  t.equals(monogamy.has('john'), false);
+  t.equals(monogamy.hasValue('mary'), false);
+  t.end();
 });
 
 test('it should delete by value', function (t) {
-    var monogamy = makeOne()
-    monogamy.deleteValue('alice')
-    t.equals(monogamy.has('bob'), false);
-    t.equals(monogamy.hasValue('alice'), false);
-    t.end();
+  var monogamy = makeOne()
+  monogamy.deleteValue('alice')
+  t.equals(monogamy.has('bob'), false);
+  t.equals(monogamy.hasValue('alice'), false);
+  t.end();
 });
 
 test('it should return the entries', function (t) {
-    var monogamy = makeOne()
-    var entriesIt = monogamy.entries()
-    t.equals(entriesIt.next().value[0], 'john');
-    t.equals(entriesIt.next().value[1], 'alice');
-    t.end();
+  var monogamy = makeOne()
+  var entriesIt = monogamy.entries()
+  t.equals(entriesIt.next().value[0], 'john');
+  t.equals(entriesIt.next().value[1], 'alice');
+  t.end();
 });
 
 test('it should check if it has a key', function (t) {
-    var monogamy = makeOne()
-    t.equals(monogamy.has('bob'), true);
-    t.equals(monogamy.has('bill'), false);
-    t.end();
+  var monogamy = makeOne()
+  t.equals(monogamy.has('bob'), true);
+  t.equals(monogamy.has('bill'), false);
+  t.end();
 });
 
 test('it should check if it has a value', function (t) {
-    var monogamy = makeOne()
-    t.equals(monogamy.hasValue('alice'), true);
-    t.equals(monogamy.hasValue('laura'), false);
-    t.end();
+  var monogamy = makeOne()
+  t.equals(monogamy.hasValue('alice'), true);
+  t.equals(monogamy.hasValue('laura'), false);
+  t.end();
 });
 
 
 test('it should return the keys', function (t) {
-    var monogamy = makeOne()
-    var keysIt = monogamy.keys()
-    t.equals(keysIt.next().value, 'john');
-    t.equals(keysIt.next().value, 'bob');
-    t.end();
+  var monogamy = makeOne()
+  var keysIt = monogamy.keys()
+  t.equals(keysIt.next().value, 'john');
+  t.equals(keysIt.next().value, 'bob');
+  t.end();
 });
 
 test('it should return the values', function (t) {
-    var monogamy = makeOne()
-    var valuesIt = monogamy.values()
-    t.equals(valuesIt.next().value, 'mary');
-    t.equals(valuesIt.next().value, 'alice');
-    t.end();
+  var monogamy = makeOne()
+  var valuesIt = monogamy.values()
+  t.equals(valuesIt.next().value, 'mary');
+  t.equals(valuesIt.next().value, 'alice');
+  t.end();
 });
 
 test('it should return the plain {}', function (t) {
-    var monogamy = makeOne()
-    var object = monogamy.getObject()
-    t.equals(typeof object, 'object');
-    t.equals(object['john'], 'mary');
-    t.equals(object['bob'], 'alice');
-    t.end();
+  var monogamy = makeOne()
+  var object = monogamy.getObject()
+  t.equals(typeof object, 'object');
+  t.equals(object['john'], 'mary');
+  t.equals(object['bob'], 'alice');
+  t.end();
 });
 
 test('it should return the reverse plain {}', function (t) {
-    var monogamy = makeOne()
-    var object = monogamy.getObjectReverse()
-    t.equals(typeof object, 'object');
-    t.equals(object['mary'], 'john');
-    t.equals(object['alice'], 'bob');
-    t.end();
+  var monogamy = makeOne()
+  var object = monogamy.getObjectReverse()
+  t.equals(typeof object, 'object');
+  t.equals(object['mary'], 'john');
+  t.equals(object['alice'], 'bob');
+  t.end();
 });
